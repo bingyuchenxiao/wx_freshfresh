b0e19c6f4b2321c1060381319ef11b71bca24665
dengwei <549448428@qq.com>
2017-01-08 14:15:58 +0800

    tel

--- page/component/category/index.js
+++ page/component/category/index.js
@@ -1,9 +1,11 @@
 // page/component/category/index.js
 Page({
-  data:{},
+  data:{
+    homepage:{}
+  },
   onLoad:function(options){
     // 页面初始化 options为页面跳转所带来的参数
-    console.log('categoty')
+    console.log(options)
   },
   onReady:function(){
     // 页面渲染完成
